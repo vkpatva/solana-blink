a6df6f3333a8bf700e1cee280631af811c743651
vkpatva <vkpatva.it@gmail.com>
2024-08-25 20:50:39 +0530

    fix: updated code

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -1,21 +1,26 @@
 import { createActionHeaders, type ActionsJson } from "@solana/actions";
 
 export const GET = async () => {
-    const payload: ActionsJson = {
-        rules: [
-            {
-                pathPattern: "/*",
-                apiPath: "/api/actions/*",
-            },
-            {
-                pathPattern: "/api/actions/**",
-                apiPath: "/api/actions/**",
-            },
-        ],
-    };
+  const payload: ActionsJson = {
+    rules: [
+      // map all root level routes to an action
+      {
+        pathPattern: "/*",
+        apiPath: "/api/actions/*",
+      },
+      // idempotent rule as the fallback
+      {
+        pathPattern: "/api/actions/**",
+        apiPath: "/api/actions/**",
+      },
+    ],
+  };
 
-    return Response.json(payload, {
-        headers: createActionHeaders(),
-    });
+  return Response.json(payload, {
+    headers: createActionHeaders(),
+  });
 };
+
+// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+// THIS WILL ENSURE CORS WORKS FOR BLINKS
 export const OPTIONS = GET;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,161 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  CoinsIcon,
+  FileTextIcon,
+  ImageIcon,
+  ShieldIcon,
+  WalletIcon,
+} from "lucide-react";
+
+const actionCards: Array<{
+  title: string;
+  href: string;
+  description: React.ReactNode;
+  icon: React.ReactNode;
+}> = [
+  {
+    title: "On-chain Memo",
+    href: "/memo",
+    description: "Send a simple message on-chain using an SPL Memo.",
+    icon: <FileTextIcon className="size-12" />,
+  },
+  {
+    title: "Staking SOL",
+    href: "/stake",
+    description:
+      "Help secure the Solana network by staking SOL to a validator.",
+    icon: <ShieldIcon className="size-12" />,
+  },
+  {
+    title: "Transfer Native SOL",
+    href: "/transfer-sol",
+    description: "Easily transfer native SOL to any other Solana wallet.",
+    icon: <WalletIcon className="size-12" />,
+  },
+  // {
+  //   title: "Transfer SPL Tokens",
+  //   href: "/transfer-spl",
+  //   description: "Easily transfer SPL tokens to any other Solana wallet.",
+  //   icon: <CoinsIcon className="size-12" />,
+  // },
+  // {
+  //   title: "Mint an NFT",
+  //   href: "/mint-nft",
+  //   description:
+  //     "Allow anyone to claim a digital collectible from a Collection.",
+  //   icon: <ImageIcon className="size-12" />,
+  // },
+];
+
+export default async function Pages() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    <>
+      {/* <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:pt-32">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <Link
+            href={siteConfig.links.twitter}
+            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Follow along on Twitter
+          </Link>
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            An example app built using Next.js 13 server components.
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            I&apos;m building a web app with Next.js 13 and open sourcing
+            everything. Follow along as we figure this out together.
+          </p>
+          <div className="space-x-4">
+            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+              Get Started
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+            >
+              GitHub
+            </Link>
+          </div>
         </div>
-      </div>
+      </section> */}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      <section
+        id="features"
+        className={
+          "container space-y-12 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
+        }
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-6 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Example Solana Actions
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            This project contains examples code snippets for creating Solana
+            Actions.
           </p>
-        </a>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+          {actionCards.map((item, key) => (
+            <Link key={key} href={item.href} className="group">
+              <Card className="group-hover:border-primary">
+                <CardHeader>
+                  <CardTitle className="space-y-3">
+                    {item.icon}
+                    <span className="block font-bold group-hover:text-pretty">
+                      {item.title}
+                    </span>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <p className="text-muted-foreground">{item.description}</p>
+                </CardContent>
+              </Card>
+            </Link>
+          ))}
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
+        <div className="mx-auto text-center md:max-w-[58rem]">
+          <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            You can find the{" "}
+            <Button variant={"link"} asChild>
+              <Link href={siteConfig.links.github} target="_blank">
+                full source code
+              </Link>
+            </Button>{" "}
+            for this entire repo on GitHub.
           </p>
-        </a>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      {/* <section id="open-source" className="container py-8 md:py-12 lg:py-24">
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Proudly Open Source
           </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            Taxonomy is open source and powered by open source software. <br />{" "}
+            The code is available on{" "}
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4"
+            >
+              GitHub
+            </Link>
+            .{" "}
           </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </section> */}
+    </>
   );
 }
